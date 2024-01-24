c1c58474806bc932804fa232d24f13eef736dc9c
JOEL <joelzapata0405@gmail.com>
2024-01-23 21:15:55 -0500

    Prueba del codigo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-let 
+var clima = prompt('Ingrese el clima');
+
+if (clima === 'lluvioso') {
+    console.log('Llevar sombrilla');
+} else if (clima === 'soleado') {
+    console.log('Relax');
+} else if (clima === 'nublado') {
+    console.log('Clima sabroso');
+} else {
+    console.log('No reconozco ese tipo de clima');
+}
