13beac9db969e2ee333552cc84a4ebdd4c57386e
JOEL <joelzapata0405@gmail.com>
2024-01-28 13:06:01 -0500

    Conversor de moneda con HTML, CSS Y JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-var clima = prompt('Ingrese el clima');
+function convertir() {
+    var valores = parseInt(document.getElementById('valor').value);
+    var resultado = 0;
+    var dolar = 3939;
+    var euro = 4279;
 
-if (clima === 'lluvioso') {
-    console.log('Llevar sombrilla');
-} else if (clima === 'soleado') {
-    console.log('Relax');
-} else if (clima === 'nublado') {
-    console.log('Clima sabroso');
-} else {
-    console.log('No reconozco ese tipo de clima');
+    if (document.getElementById("uno").checked) {
+        resultado = valores / dolar;
+        alert("El cambio de pesos a dólares es: $" + resultado.toFixed(2));
+    } else if (document.getElementById("dos").checked) {
+        resultado = valores / euro;
+        alert("El cambio de pesos a Euros es: " + resultado.toFixed(2));
+    } else {
+        alert("Tienes que completar todos los requerimientos");
+    }
 }
