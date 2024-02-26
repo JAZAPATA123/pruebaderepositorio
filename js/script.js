be81a36aa4d5290aba4c933211867a714817410d
JAZAPATA123 <148886702+JAZAPATA123@users.noreply.github.com>
2024-02-26 15:45:50 -0500

    script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,69 @@
-function convertir() {
-    var valores = parseInt(document.getElementById('valor').value);
-    var resultado = 0;
-    var dolar = 3939;
-    var euro = 4279;
 
-    if (document.getElementById("uno").checked) {
-        resultado = valores / dolar;
-        alert("El cambio de pesos a dólares es: $" + resultado.toFixed(2));
-    } else if (document.getElementById("dos").checked) {
-        resultado = valores / euro;
-        alert("El cambio de pesos a Euros es: " + resultado.toFixed(2));
-    } else {
-        alert("Tienes que completar todos los requerimientos");
-    }
+const baseDeDatos = [
+	{
+		id: 1,
+		nombre: 'Desarrollo de página web',
+		precio: 500,
+		imagen: 'producto1.jpg',
+        estilo: 'producto1'
+	},
+	{
+		id: 2,
+		nombre: 'Diseño gráfico',
+		precio: 300,
+		imagen: 'producto2.jpg',
+        estilo: 'producto2'
+
+	},
+	{
+		id: 3,
+		nombre: 'Desarrollo de aplicación móvil',
+		precio: 800,
+		imagen: 'producto3.jpg',
+        estilo: 'producto3'
+
+	}
+];
+
+
+const items = document.getElementById('items');
+const carrito = document.getElementById('carrito');
+const total = document.getElementById('total');
+const botonVaciar = document.getElementById('boton-vaciar');
+
+function renderizarProductos() {
+	baseDeDatos.forEach(producto => {
+		const div = document.createElement('div');
+		div.classList.add('producto');
+		div.innerHTML = `
+			<img src="${producto.imagen}" alt="${producto.nombre}">
+			<h3>${producto.nombre}</h3>
+			<p>$${producto.precio}</p>
+			<button onclick="agregarAlCarrito(${producto.id})">Agregar al carrito</button>
+		`;
+		items.appendChild(div);
+	});
 }
+
+function agregarAlCarrito(id) {
+	const producto = baseDeDatos.find(producto => producto.id === id);
+	const listaProductos = document.createElement('li');
+	listaProductos.textContent = `${producto.nombre} - $${producto.precio}`;
+	carrito.appendChild(listaProductos);
+	actualizarTotal(producto.precio);
+}
+
+function actualizarTotal(precio) {
+	const totalActual = parseInt(total.textContent);
+	total.textContent = totalActual + precio;
+}
+
+function vaciarCarrito() {
+	while (carrito.firstChild) {
+		carrito.removeChild(carrito.firstChild);
+	}
+	total.textContent = 0;
+}
+
+botonVaciar.addEventListener('click', vaciarCarrito);
+renderizarProductos();
